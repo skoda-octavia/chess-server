d742cfbaed4ad706573e0345b44d0d83e3049692
piotras <piotr.kow.59@gmail.com>
2024-08-23 23:26:41 +0200

    chess board integrated

--- chessboard/src/app/app.component.ts
+++ chessboard/src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {NgxChessBoardService} from 'ngx-chess-board';
+import {NgxChessBoardView} from 'ngx-chess-board';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'chessboard';
+export class AppComponent implements OnInit {
+  @ViewChild('board', { static: false })
+  board!: NgxChessBoardView;
+
+  turn = true;
+  isGameFinished = false;
+  endgameMessage = '';
+
+  constructor(private ngxChessBoardService: NgxChessBoardService) {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this.board.reset();
+  }
+
+  switchPlayerTurn() {
+    this.turn = !this.turn;
+  }
+
+  MoveCompleted(event: any) {
+    console.log(event)
+    if (event.checkmate && this.turn) {
+      this.endgameMessage = 'white won!';
+      this.isGameFinished = true;
+      console.warn(this.endgameMessage)
+      return;
+    }
+
+    if (event.stalement) {
+      this.endgameMessage = 'draw!';
+      this.isGameFinished = true;
+      console.warn(this.endgameMessage)
+      return;
+    }
+    console.log(this.board.getFEN());
+    this.switchPlayerTurn();
+  }
+
+  onResetGame() {
+    this.board.reset();
+    this.turn = true;
+    this.endgameMessage = '';
+    this.isGameFinished = false;
+  }
 }
--- chessboard/src/app/app.module.ts
+++ chessboard/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxChessBoardModule } from "ngx-chess-board";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    NgxChessBoardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
