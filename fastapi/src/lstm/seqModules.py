adf81cef62dcd12cb42cffcd634d1cde62753e62
piotras <piotr.kow.59@gmail.com>
2024-09-05 21:12:58 +0200

    lstm service

---
+++ fastapi/src/lstm/seqModules.py
@@ -0,0 +1,111 @@
+import torch
+import torch.nn as nn
+import random
+from torch.utils.data import DataLoader, Dataset
+import torch.nn.functional as F
+
+
+class SequenceDataset(Dataset):
+    def __init__(self, src_sequences, tar_sequences, legals, src_padd_idx, tar_padd_idx, max_src_len, max_tar_len):
+        self.src_sequences = [torch.tensor(seq) for seq in src_sequences]
+        self.tar_sequences = [torch.tensor(seq) for seq in tar_sequences]
+        self.legals = [torch.tensor(legals[i]) for i in range(len(legals))]
+        self.src_padd_idx = src_padd_idx
+        self.tar_padd_idx = tar_padd_idx
+        self.max_src_len = max_src_len
+        self.max_tar_len = max_tar_len
+
+    def __len__(self):
+        return len(self.src_sequences)
+
+    def __getitem__(self, idx):
+        src_seq = self.src_sequences[idx]
+        tar_seq = self.tar_sequences[idx]
+        legals = self.legals[idx]
+
+        src_seq = torch.nn.functional.pad(src_seq, (0, self.max_src_len - len(src_seq)), value=self.src_padd_idx)
+        tar_seq = torch.nn.functional.pad(tar_seq, (0, self.max_tar_len - len(tar_seq)), value=self.tar_padd_idx)
+
+        return src_seq, tar_seq, legals
+
+class Encoder(nn.Module):
+    def __init__(self, input_size, embed_size, hidden_size, num_layers, drop, src_pad):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.dropout = nn.Dropout(drop)
+        self.embed = nn.Embedding(input_size, embed_size, src_pad)
+        self.rnn = nn.LSTM(embed_size, hidden_size, num_layers, dropout=drop)
+
+    def forward(self, x):
+        embed = self.dropout(self.embed(x))
+        out, (hidden, cell) = self.rnn(embed)
+        return hidden, cell
+
+class Decoder(nn.Module):
+    def __init__(self, input_size, embed_size, hidden_size, output_size, num_layers, drop, tar_pad):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.rnn = nn.LSTM(embed_size, hidden_size, num_layers, dropout=drop)
+        self.fc = nn.Linear(hidden_size, output_size)
+        self.dropout = nn.Dropout(drop)
+        self.embed = nn.Embedding(input_size, embed_size, tar_pad)
+        self.acc = nn.ReLU()
+
+
+    def forward(self, x, hidden, cell):
+        x = x.unsqueeze(0)
+        embed = self.dropout(self.embed(x))
+
+        out, (hidden, cell) = self.rnn(embed, (hidden, cell))
+        preds = self.fc(out)
+        return preds.squeeze(0), hidden, cell
+
+
+class Seq2Seq(nn.Module):
+    def __init__(self, src_vocab_len, tar_vocab_len, embed, hidden, layers, drop, src_pad, tar_pad):
+        super().__init__()
+        self.encoder = Encoder(src_vocab_len, embed, hidden, layers, drop, src_pad)
+        self.decoder = Decoder(tar_vocab_len, embed, hidden, tar_vocab_len, layers, drop, tar_pad)
+        self.tar_vocab_len = tar_vocab_len
+
+    def forward(self, seq, tar, teach_force=0.5, mask=None):
+        batch_size = seq.shape[1]
+        tar_len = tar.shape[0]
+        
+        if mask is not None and batch_size != 1:
+            raise ValueError("Mask was given but batch size was not size 1")
+
+        hidden, cell = self.encoder(seq)
+        outputs = torch.zeros(tar_len, batch_size, self.tar_vocab_len).to("cuda")
+        x = tar[0]
+        for i in range(1, tar_len):
+            output, hidden, cell = self.decoder(x, hidden, cell)
+            
+            if mask is not None:
+                output = F.softmax(output, -1)
+                output = self.filter_output(output, mask, i-1)
+                best = output.argmax(1)
+                mask = self.adjust_mask(mask, best, i-1)
+            else:
+                best = output.argmax(1)
+
+            outputs[i] = output
+            x = tar[i] if random.random() < teach_force else best
+        return outputs
+    
+
+    def filter_output(self, output, mask, idx):
+        output = torch.squeeze(output)
+        move_mask = mask[:, idx, :]
+        max_values, _ = torch.max(move_mask, dim=0)
+        output *= max_values
+        return torch.unsqueeze(output, 0)
+    
+    def adjust_mask(self, mask, best, idx):
+        selected = best[0].item()
+        check = mask[:, idx, selected] == 1
+        mask[~check] = 0
+
+        return mask
---
+++ fastapi/src/lstm/service.py
@@ -0,0 +1,116 @@
+import torch
+import torch.nn as nn
+import json
+from torch.utils.data import DataLoader
+import torch.optim as optim
+import torch.nn.functional as F
+import random
+from src.lstm.seqModules import Seq2Seq
+import chess
+
+class LstmService():
+
+    def __init__(self) -> None:
+            
+        vocab_path = "src/lstm/vocab_src.json"
+        tar_vocab_path = "src/lstm/vocab_tar.json"
+        with open(vocab_path, "r") as f:
+            self.vocab = json.load(f)
+            self.reverse_vocab = {v: k for k, v in self.vocab.items()}
+        with open(tar_vocab_path, "r") as f:
+            self.tar_vocab = json.load(f)
+            self.reverse_tar_vocab = {v: k for k, v in self.tar_vocab.items()}
+        self.src_pad = len(self.vocab)
+        self.tar_pad = len(self.tar_vocab)
+
+        self.src_vocab_len = self.src_pad + 1
+        self.tar_vocab_len = self.tar_pad + 1
+
+        device = torch.device("cuda")
+        embed = 512
+        hidden = 512
+        layers = 4
+        drop = 0
+
+        self.model = Seq2Seq(
+            self.src_vocab_len,
+            self.tar_vocab_len,
+            embed,
+            hidden,
+            layers,
+            drop,
+            self.src_pad,
+            self.tar_pad
+            ).to(device)
+        self.model.eval()
+        self.model.load_state_dict(torch.load("models/lstm49.pth", weights_only=True))
+
+    def tokenize_fen(self, fen: str):
+        tokens = [self.vocab['SOS']]
+        fen_splited = fen.split(" ")
+        assert len(fen_splited) == 6
+        board, turn_str, cas, en_pass, _, _ = fen_splited 
+
+        nums_to_change = [8, 7, 6, 5, 4, 3, 2]
+        for num in nums_to_change:
+            board = board.replace(str(num), num * "1")
+        for char in board:
+            tokens.append(self.vocab[char])
+
+        turn = "True" if turn_str == "w" else "False"
+        tokens.append(self.vocab[turn])
+
+        if cas != "-":
+            cas = cas.replace("K", "Ki")
+            cas = cas.replace("Q", "Qu")
+            cas = cas.replace("k", "ki")
+            cas = cas.replace("q", "qu")
+            elems = [cas[i:i+2] for i in range(0, len(cas), 2)]
+            tokens.extend([self.vocab[cr] for cr in elems])
+
+        if en_pass != "-":
+            en = en_pass[0] + "_enpas"
+            tokens.append(self.vocab[en])
+        tokens.append(self.vocab["EOS"])
+
+        return tokens
+    
+    def create_tar_mask(self, fen: str):
+        board = chess.Board(fen)
+        legal_moves = list(board.legal_moves)
+        move_tokens = []
+        for move in legal_moves:
+            from_, to = chess.square_name(move.from_square), chess.square_name(move.to_square)
+            temp = [self.tar_vocab[from_], self.tar_vocab[to]]
+            prom = self.tar_vocab[move.uci()[4]] if move.promotion is not None else self.tar_vocab["EOS"]
+            temp.append(prom)
+            move_tokens.append(temp)
+        return move_tokens
+    
+
+    def __call__(self, fen: str):
+        tokenized_fen = self.tokenize_fen(fen)
+        tar_mask = self.create_tar_mask(fen)
+        tensor = torch.tensor(tar_mask)
+        max_idx = self.tar_pad
+        one_hot_tensor = torch.zeros(tensor.size(0), tensor.size(1), max_idx + 1)
+        one_hot_tensor.scatter_(2, tensor.unsqueeze(-1), 1)
+
+        
+        fake_output = torch.tensor([0, 38, 30, 1])
+        fake_output = torch.unsqueeze(fake_output, 0)
+        fake_output = fake_output.permute(1, 0).to("cuda")
+        input = torch.tensor(tokenized_fen)
+        input = torch.unsqueeze(input, 0)
+        input = input.permute(1, 0).to("cuda")
+        one_hot_tensor = one_hot_tensor.to("cuda")
+        output = self.model(input, fake_output, 1, one_hot_tensor)
+        output_class = output.permute(1, 0, 2)
+        output_moves = torch.argmax(output_class, dim=2)[:, 1:]
+        tokens = torch.squeeze(output_moves).tolist()
+
+        eos_token = self.tar_vocab["EOS"]
+        move_symbols = [self.reverse_tar_vocab[token] for token in tokens if token != eos_token]
+
+        print(move_symbols)
+        return "".join(move_symbols)
